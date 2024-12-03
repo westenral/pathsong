41fccf0ab3694e0adb9c348b0d6e059ae3db6ca7
westenral <137741690+westenral@users.noreply.github.com>
2024-12-02 19:14:55 -0500

    add outline for core songgraph methods

--- include/songgraph.h
+++ include/songgraph.h
@@ -6,13 +6,25 @@
 #define PATHSONG_SONGGRAPH_H
 
 
+#include "types.h"
+
 #include <string>
 #include <vector>
+
 class SongGraph {
+private:
+
+    // return the edge weight between the two vertices
+    // edge weight is identical to similarity score (`Song::similarity`)
+    u8 edge(size_t v1, size_t v2);
+
+    std::vector<size_t> get_path(size_t song1, size_t song2);
+
 public:
+
     // the primary function of the SongGraph. Given two names, finds the best
     // path between them.
-    std::vector <std::string> get_path(std::string &song_1, std::string &song_2);
+    std::vector<std::string> get_path(std::string &song1, std::string &song2);
 };
 
 
--- src/songgraph.cpp
+++ src/songgraph.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "../include/songgraph.h"
+
+std::vector<std::string> SongGraph::get_path(std::string &song1, std::string &song2) {
+    // lookup song ids from name
+    // traverse internal graph to find shortest path
+}
+
+std::vector<size_t> SongGraph::get_path(size_t song1, size_t song2) {
+    // priority queue of edge weights
+    // whether a vertex has been visited
+    // predecessors
+
+    // pick the smallest edge
+    // if we found the target - check through predecessors to get path
+}
